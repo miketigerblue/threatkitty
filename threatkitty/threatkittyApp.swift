313eb7444044514d8ce1a39f61696b0551e3aa07
Mike Harris <mike@tigerblue.io>
2025-05-04 11:21:33 +0100

    Initial Commit

---
+++ threatkitty/threatkittyApp.swift
@@ -0,0 +1,17 @@
+//
+//  threatkittyApp.swift
+//  threatkitty
+//
+//  Created by Mike Harris on 04/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct threatkittyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ threatkittyTests/threatkittyTests.swift
@@ -0,0 +1,17 @@
+//
+//  threatkittyTests.swift
+//  threatkittyTests
+//
+//  Created by Mike Harris on 04/05/2025.
+//
+
+import Testing
+@testable import threatkitty
+
+struct threatkittyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
